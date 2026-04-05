e4c52ef3c96364d167808e734929129e4bced1b6
Mateusz <mateusz.ruszczynski@gmail.com>
2026-04-05 15:03:21 +0200

    feat: create forms.py

---
+++ manager/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Task, Tag
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Enter tag name...',
+            }),
+        }
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['content', 'deadline', 'tags']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'What needs to be done?',
+            }),
+            'deadline': forms.DateTimeInput(),
+            'tags': forms.CheckboxSelectMultiple(),
+        }
